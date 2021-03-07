dd4552713e64344d4d3dedc39b2fd627681b5c6a
OPSRv <opsrv1@gmail.com>
2021-03-08 01:42:59 +0200

    1

--- js/app.js
+++ js/app.js
@@ -1,47 +1,48 @@
 let main = document.querySelector(".main");
-let facebook = document.querySelector("#facebook");
+
+const animate = {
+  duration: 100,
+  type: "sync",
+  animTimingFunction: Vivus.EASE_IN,
+};
 
 setTimeout(function () {
   main.classList.add("main_visible");
 }, 500);
 
-facebook.addEventListener("click", () => {
-  console.log("object");
-});
-
 window.onload = function () {
   const youtubeIcon = new Vivus("youtube", {
     file: "./img/youtube.svg",
-    duration: 200,
+    ...animate,
   });
 
   const facebookIcon = new Vivus("facebook", {
-    duration: 200,
+    ...animate,
     file: "./img/facebook.svg",
   });
 
   const soundcloudIcon = new Vivus("soundcloud", {
     file: "./img/soundcloud.svg",
-    duration: 200,
+    ...animate,
   });
   const whatsappIcon = new Vivus("whatsapp", {
     file: "./img/whatsapp.svg",
-    duration: 200,
+    ...animate,
   });
   const telegramIcon = new Vivus("telegram", {
     file: "./img/telegram.svg",
-    duration: 200,
+    ...animate,
   });
   const gmailIcon = new Vivus("gmail", {
     file: "./img/gmail.svg",
-    duration: 200,
+    ...animate,
   });
   const instagramIcon = new Vivus("instagram", {
     file: "./img/instagram.svg",
-    duration: 200,
+    ...animate,
   });
   const youtube_сopyIcon = new Vivus("youtube_сopy", {
     file: "./img/youtube_сopy.svg",
-    duration: 200,
+    ...animate,
   });
 };
